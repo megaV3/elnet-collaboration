18fb572df3b17348abfa3a8e3094ab151cbf9832
pospas <pospas@DESKTOP-GV8A259>
2026-04-25 20:47:01 +0800

    Add project files.

---
+++ ELNET_FinalsProject/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace ELNET_FinalsProject.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
